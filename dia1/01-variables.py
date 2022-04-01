23c55d4d94eeec092e8b0ba23591e05ee21e9e93
Ronald Beltran <rbeltrann@gmail.com>
2022-04-01 18:34:46 -0500

    declaracion de los primeros archivos

---
+++ dia1/01-variables.py
@@ -0,0 +1,19 @@
+# esto es un comentario
+edad=30
+
+
+nombre='eduardo'
+apellido='beltran'
+
+descripcion='''hola:
+como estan?
+yo muy bien
+'''
+print(descripcion)
+
+print('a','b','c')
+
+year=2022
+
+print(type(year))
+
