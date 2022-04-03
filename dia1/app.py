06a6029d2988269dd7c4171bd315ae97d09f2515
Ronald Beltran <rbeltrann@gmail.com>
2022-04-03 00:37:02 -0500

    flask

--- dia1/app.py
+++ dia1/app.py
@@ -1,5 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from datetime import datetime
 
 app=Flask(__name__)
+
+clientes=[]
+
+@app.route('/')
+def estado():
+    hora_del_servidor=datetime.now()
+
+    return{
+        'status':True,
+        'hour': hora_del_servidor.strftime('%d/%m/%Y %H:%M:%S')
+    }
+
+@app.route('/clientes', methods=['POST'])
+def obtener_clientes():
+    # sólo puede ser llamado en cada controlador
+    print(request.method)
+    print(request.get_json())
+    data=request.get_json()
+    clientes.append(data)
+
+    return{
+        'message':'cliente agregado exitosamente',
+        'client': data
+    }
+
 
 app.run(debug=True)
