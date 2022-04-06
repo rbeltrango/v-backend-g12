0d7e8233dbddda6d46970f2d0fa2ec7d97e21c9d
Ronald Beltran <rbeltrann@gmail.com>
2022-04-05 21:21:43 -0500

    migracioens

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -12,7 +12,33 @@ class Etiqueta(models.Model):
     class Meta:
         db_table='etiquetas'
         # modficando el ordenameinto ascendeten se le pone menos ('-nombre')para que sea descendente
-        ordering=['nombre'] 
+        ordering=['-nombre']
 
-        
+class Tareas(models.Model):
 
+    # CADA opcion le podemos colocar dos parametros. El primero será una abreviatura para que se guarde en la BD y el segundo el nombre completo
+    class CategoriaOpciones(models.TextChoices):
+        TODO='TODO', 'TO_DO'
+        IN_PROGRESS='IP', 'IN_PROGRESS'
+        DONE='DONE','DONE'
+        CANCELLED='CANCELLED', 'CANCELLED'
+
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=45, null=False)
+    categoria=models.CharField(max_length=45, choices=CategoriaOpciones.choices,default=CategoriaOpciones.TODO)
+
+    fechaCaducidad=models.DateTimeField(db_column='fecha_caducidad')
+    importancia=models.IntegerField(null=False)
+
+    createdAt=models.DateTimeField(auto_now_add=True, db_column='created_at')
+    updatedAt=models.DateTimeField(auto_now=True, db_column='updated_at')
+
+# vamos a crear una relacion de muchos a muchos
+# en Django se puede realizar las relaciones one-to-one, one-to-many o many-to-many
+# para crear las relaciones entre tablas, acá ya no es necesario crear las relationships
+# porque ya están integradas dentro de la relacion
+    etiquetas=models.ManyToManyField(to=Etiqueta, related_name='tareas')
+
+    class Meta:
+        db_table='tareas'
+       
