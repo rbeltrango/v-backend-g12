d0e691fe66b6f4a024a76801f4bac99e250a393c
Ronald Beltran <rbeltrann@gmail.com>
2022-04-10 13:29:10 -0500

    creacion de controladores

--- agenda/gestion/serializers.py
+++ agenda/gestion/serializers.py
@@ -1,5 +1,8 @@
-from unittest.util import _MAX_LENGTH
+# from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
+from .models import Tareas
+
+# from agenda.gestion.models import Tareas
 # https://www.django-rest-framework.org/api-guide/serializers/
 # https://www.django-rest-framework.org/api-guide/fields/
 
@@ -9,3 +12,9 @@ class PruebaSerializers(serializers.Serializer):
     correo=serializers.EmailField()
     dni=serializers.RegexField(max_length=8, min_length=8, regex="[0-9")
     # dni = serializers.IntegerField(min_value=10000000, max_value=99999999)  
+
+class TareaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Tareas
+        fields= '__all__'
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from .serializers import PruebaSerializers, TareaSerializer
+from .models import Tareas
 
 @api_view(http_method_names=['GET','POST'])
 def inicio(request: Request):
@@ -21,5 +23,34 @@ def inicio(request: Request):
         }, status=201)
         
 class PruebaApiView(ListAPIView):
-    serializer_class=None
+    serializer_class=PruebaSerializers
+
+
+    queryset = [{
+        'nombre':'Eduardo', 
+        'apellido': 'De Rivero', 
+        'correo':'ederiv@gmail.com',
+        'dni':'73500746', 
+        'estado_civil':'viudo'},
+        {
+        'nombre':'maria', 
+        'apellido': 'perez', 
+        'correo':'ederiv@gmail.com',
+        'dni':'73500746', 
+        'estado_civil':'casado'}]
+
+    def get(self, request:Request):
+        informacion=self.queryset
+
+        informacion_serializada=self.serializer_class(data=informacion, many=True)
+
+        informacion_serializada.is_valid(raise_exception=True)
+        return Response(data={
+            'message':'Hola',
+            'content': informacion_serializada.data
+            })
     
+class TareasApiView(ListCreateAPIView):
+    queryset=Tareas.objects.all() # SELECT * from TAREAS
+    serializer_class=TareaSerializer
+
