97e14278466d914dd539ba3bd275032ab5a6c182
Ronald Beltran <rbeltrann@gmail.com>
2022-04-11 20:03:31 -0500

    semana 07

--- agenda/agenda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-# include sirve para incluir un archivo con varias rutas al archivo de rutas del proyecto
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('gestion/', include('gestion.urls'))   
-]
