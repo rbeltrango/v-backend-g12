982fb449c9f5266193361be709f39bfba8a41986
Ronald Beltran <rbeltrann@gmail.com>
2022-04-25 22:34:06 -0500

    limpieaa de rama

--- fact_electr/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import URLPattern, path
-from .views import GenerarComprobanteApiView
-
-urlpatterns=[
-    path('generar-comprobante/', GenerarComprobanteApiView.as_view()),
-]
--- menu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PedidoApiView, PlatoApiView, StockApiView, AgregarDetallePedidoApiView
-
-urlpatterns=[
-    path('', PlatoApiView.as_view()),
-    path('stock/', StockApiView.as_view()),
-    path('pedido/', PedidoApiView.as_view()),
-    path('agregar-detalle/',AgregarDetallePedidoApiView.as_view())
-]
